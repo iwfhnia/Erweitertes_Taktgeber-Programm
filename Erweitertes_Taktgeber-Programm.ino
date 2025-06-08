8745a3b58a2c9972926768ed432b4b062d50d9b1
iwfhnia <krausesamu@gmail.com>
2025-06-08 19:39:56 +0200

    Update Erweitertes_Taktgeber-Programm.ino

--- Erweitertes_Taktgeber-Programm.ino
+++ Erweitertes_Taktgeber-Programm.ino
@@ -1,19 +1,25 @@
-// Arduino: zwei Taktsignale (1 Hz und 0,5 Hz) und Ausgang Z plotten
+// Arduino: drei Taktsignale (Eingänge: B,C,D) und Ausgang Z plotten
 
 const int clk1Pin = 2;   // 1 Hz → D2
-const int clk2Pin = 4;   // 0,5 Hz → D4 (doppelte Periodendauer)
-const int zPin     = 3;   // D3 zum Einlesen von Z
+const int clk2Pin = 3;   // 0,5 Hz → D3
+const int clk3Pin = 4;   // 0,25 Hz → D4
+const int zPin     = 5;   // D5 zum Einlesen von Z
 
 bool clk1State = false;
 bool clk2State = false;
+bool clk3State = false;
 unsigned long lastToggle1 = 0;
 unsigned long lastToggle2 = 0;
+unsigned long lastToggle3 = 0;
 const unsigned long interval1 = 500;   // 500 ms high/low → 1 Hz
 const unsigned long interval2 = 1000;  // 1000 ms high/low → 0,5 Hz
+const unsigned long interval3 = 2000;  // 2000 ms high/low → 0,25 Hz
+
 
 void setup() {
   pinMode(clk1Pin, OUTPUT);
   pinMode(clk2Pin, OUTPUT);
+  pinMode(clk3Pin, OUTPUT);
   pinMode(zPin, INPUT);
   Serial.begin(9600);
 }
@@ -28,11 +34,18 @@ void loop() {
     lastToggle1 = now;
   }
 
-  // 0,5 Hz-Taktsignal (D4)
+  // 0,5 Hz-Taktsignal (D3)
   if (now - lastToggle2 >= interval2) {
     clk2State = !clk2State;
     digitalWrite(clk2Pin, clk2State);
     lastToggle2 = now;
+  }
+
+    // 0,25 Hz-Taktsignal (D4)
+  if (now - lastToggle3 >= interval3) {
+    clk3State = !clk3State;
+    digitalWrite(clk3Pin, clk3State);
+    lastToggle3 = now;
   }
 
   // Ausgang Z einlesen
@@ -43,7 +56,9 @@ void loop() {
   Serial.print(' ');
   Serial.print(clk2State);
   Serial.print(' ');
+  Serial.print(clk3State);
+  Serial.print(' ');
   Serial.println(z);
 
-    delay(50);  // Kleine Verzögerung für saubere Darstellung
+    delay(100);  // Verzögerung für Darstellung einer ganzen Periode
 }
